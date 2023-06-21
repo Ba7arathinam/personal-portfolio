d0c1c8d1a893ecba8859212b093a21ad044f9ba7
Ba7arathinam <bala01225@gmail.com>
2023-06-21 20:29:26 +0530

    conit it

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,6 +1,7 @@
-import Carousel from 'react-multi-carousel';
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import 'react-multi-carousel/lib/styles.css';
 import colorSharp from "../assets/img/color-sharp.png"
+import {SkillCard} from "./skillCard"
 
 
 export const Skills = () => {
@@ -24,48 +25,86 @@ export const Skills = () => {
   //   }
   // };
 
+  const skills=[
+    {
+        img:"https://logos-download.com/wp-content/uploads/2017/07/HTML5_badge.png",
+        toolName:"HTML",
+    },
+    {
+        img:"https://devsouth.us/images/css3_badge.png",
+        toolName:"CSS",
+    },
+    {
+        img:"https://www.freepnglogos.com/uploads/javascript-png/js-logo-png-5.png",
+        toolName:"JavaScript",
+    },
+    {
+        img:"https://logos-download.com/wp-content/uploads/2016/09/React_logo_logotype_emblem.png",
+        toolName:"React.js",
+    },
+    {
+        img:"https://logos-download.com/wp-content/uploads/2016/09/Node_logo_NodeJS.png",
+        toolName:"Node.js",
+    },
+    {
+        img:"https://1000marcas.net/wp-content/uploads/2021/06/MongoDB-Logo.png",
+        toolName:"MongoDB",
+    },
+    {
+        img:"https://th.bing.com/th/id/R.8251fe76d7487a28b07603515b300964?rik=H7lrBp652zDaWg&riu=http%3a%2f%2fassets.stickpng.com%2fimages%2f5848309bcef1014c0b5e4a9a.png&ehk=N%2fi7UuvK4YyoLcxusdDEWeftPaGIfNJl2VmpTOtLt%2bA%3d&risl=&pid=ImgRaw&r=0",
+        toolName:"Redux",
+    },
+  ]
+
   return (
     <section className="skill" id="skills">
-        <div className="container">
-            <div className="row">
-                <div className="col-12">
+        <Container>
+            <Row>
+                <Col size={12}>
                     <div className="skill-bx wow zoomIn">
                         <h2>Skills</h2>
-                        
-                        <div  className="skill-slider justify-content-center align-items-center">
-                            <div className="item">
-                                <img src="https://logos-download.com/wp-content/uploads/2017/07/HTML5_badge.png" alt="Image" />
-                                <h5>HTML5</h5>
-                            </div>
-                            <div className="item">
-                                <img src="https://devsouth.us/images/css3_badge.png" alt="Image"/>
+                        {/* <div  className="skill-slider justify-content-center align-items-center"> */}
+                        <Row>
+                        {
+                          skills.map((skill, index) => {
+                            return (
+                              <SkillCard
+                                key={index}
+                                {...skill}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                            {/* <div className="item">
+                                <img src="" alt="Image"/>
                                 <h5>CSS3</h5>
                             </div>
                             <div className="item">
-                                <img src="https://www.freepnglogos.com/uploads/javascript-png/js-logo-png-5.png" alt="Image"/>
+                                <img src="" alt="Image"/>
                                 <h5>JavaScript</h5>
                             </div>
                             <div className="item">
-                                <img src="https://logos-download.com/wp-content/uploads/2016/09/React_logo_logotype_emblem.png" alt="Image"/>
+                                <img src="" alt="Image"/>
                                 <h5>ReactJS</h5>
                             </div>
                             <div className="item">
-                                <img src="https://logos-download.com/wp-content/uploads/2016/09/Node_logo_NodeJS.png" alt="Image"/>
+                                <img src="" alt="Image"/>
                                 <h5>Node.js</h5>
                             </div>
                             <div className="item">
-                                <img src="https://1000marcas.net/wp-content/uploads/2021/06/MongoDB-Logo.png" alt="Image"/>
+                                <img src="" alt="Image"/>
                                 <h5>MongoDB</h5>
                             </div>
                             <div className="item">
-                                <img src="https://th.bing.com/th/id/R.8251fe76d7487a28b07603515b300964?rik=H7lrBp652zDaWg&riu=http%3a%2f%2fassets.stickpng.com%2fimages%2f5848309bcef1014c0b5e4a9a.png&ehk=N%2fi7UuvK4YyoLcxusdDEWeftPaGIfNJl2VmpTOtLt%2bA%3d&risl=&pid=ImgRaw&r=0" alt="Image"/>
+                                <img src="" alt="Image"/>
                                 <h5>Redux</h5>
-                            </div>
-                        </div>
+                            </div> */}
+                        {/* </div> */}
                     </div>
-                </div>
-            </div>
-        </div>
+                </Col>
+            </Row>
+        </Container>
         <img className="background-image-left" src={colorSharp} alt="Image" /> 
     </section>
   )
